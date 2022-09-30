f350385ea85c1dae14b0593d0f9470aae5c49a39
Mirko Ventura <mirko.ventura87@gmail.com>
2022-09-30 16:02:18 +0200

    Update version.py

--- .github/workflows/release/version.py
+++ .github/workflows/release/version.py
@@ -12,11 +12,11 @@ REGEX_VERSION = r'^([\w]+-[\w]+-[\w]+)' #r'^[0-9]\-[0-9]\-[0-9]$'
 if __name__ == '__main__':
     lines = BRANCH_NAME.split('-', 1)
     changelog = lines[0]
-    version_name = lines[len(lines)-1]
+    version_name = lines[len(lines) - 1]
     if (changelog == version_name) :
-        version_name = '🙂'
+        version_name = 'new-release'
     name = version_name.replace(version_name + '-', '')
     version_name = version_name.replace('-', '.')
 
     print('branch_name={}'.format(name))
-    print('version={}'.format(changelog))
+    print('version={}'.format(version_name))
